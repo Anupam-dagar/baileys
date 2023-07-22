cd161774bc45bc33ad2c879df5113afef8f563fb
Anupam Dagar <dagaranupam@gmail.com>
2023-07-23 00:11:24 +0530

    add BeforeCreate and BeforeUpdate database hooks.

--- entity/base.go
+++ entity/base.go
@@ -1,16 +1,31 @@
 package entity
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
 )
 
 type BaseModel struct {
-	Id        string         `gorm:"primary_key;column:id;default:uuid_generate_v4()" json:"id"`
+	Id        string         `gorm:"primary_key;column:id;" json:"id"`
 	CreatedAt time.Time      `gorm:"column:created_at" json:"createdAt"`
 	UpdatedAt time.Time      `gorm:"column:updated_at" json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at" json:"-"`
 	CreatedBy string         `gorm:"column:created_by" json:"createdBy"`
 	UpdatedBy string         `gorm:"column:updated_by" json:"updatedBy"`
 	DeletedBy string         `gorm:"column:deleted_by" json:"deletedBy"`
+}
+
+func (bm *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
+	bm.Id = uuid.NewString()
+	bm.CreatedAt = time.Now()
+	bm.UpdatedAt = time.Now()
+
+	return
+}
+
+func (bm *BaseModel) BeforeUpdate(tx *gorm.DB) (err error) {
+	bm.UpdatedAt = time.Now()
+
+	return
 }
